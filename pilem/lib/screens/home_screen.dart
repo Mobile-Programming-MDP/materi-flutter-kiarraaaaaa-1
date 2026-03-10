680f16ee38adef81797b870f0b68ef6eb51ff6aa
kiarraaaaaa <beneciakiarra_2226240052@mhs.mdp.ac.id>
2026-03-10 10:34:18 +0700

    update p6

--- pilem/lib/screens/home_screen.dart
+++ pilem/lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pilem/models/movie.dart';
+import 'package:pilem/screens/detail_screen.dart';
 import 'package:pilem/services/api_services.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -16,21 +17,103 @@ class _HomeScreenState extends State<HomeScreen> {
   List<Movie> _trendingMovies = [];
   List<Movie> _popularMovies = [];
 
-  Future <void> _loadMovies() async{
-    final List<Map<String, dynamic>> _allMoviesData = await _apiService.getAllMovies();
-    final List<Map<String, dynamic>> _trendingMoviesData = await _apiService.getTrendingMovies();
-    final List<Map<String, dynamic>> _popularMoviesData = await _apiService.getPopularMovies();
+  Future<void> _loadMovies() async {
+    final List<Map<String, dynamic>> allMoviesData =
+        await _apiService.getAllMovies();
+    final List<Map<String, dynamic>> trendingMoviesData =
+        await _apiService.getTrendingMovies();
+    final List<Map<String, dynamic>> popularMoviesData =
+        await _apiService.getPopularMovies();
 
     setState(() {
-      
-    _allMovies = _allMoviesData.map((e)=> Movie.fromJson(e)).toList();
-    _trendingMovies = _trendingMoviesData.map((e)=> Movie.fromJson(e)).toList();
-    _popularMovies = _popularMoviesData.map((e)=> Movie.fromJson(e)).toList();
+      _allMovies = allMoviesData.map((json) => Movie.fromJson(json)).toList();
+      _trendingMovies =
+          trendingMoviesData.map((json) => Movie.fromJson(json)).toList();
+      _popularMovies =
+          popularMoviesData.map((json) => Movie.fromJson(json)).toList();
     });
   }
 
   @override
+  void initState() {
+    super.initState();
+    _loadMovies();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Pilem"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildMoviesList("All Movies", _allMovies),
+            _buildMoviesList("Trending Movies", _trendingMovies),
+            _buildMoviesList("Popular Movies", _popularMovies),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildMoviesList(String title, List<Movie> movies) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        // Menampilkan Title Kategori Movies
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            title,
+            style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        //Menapilkan thumnail dan judul movies
+        SizedBox(
+          height: 200,
+          child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: movies.length,
+              itemBuilder: (BuildContext build, int index) {
+                final Movie movie = movies[index];
+                return GestureDetector(
+                  onTap: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => DetailScreen(movie: movie),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Image.network(
+                          "https://image.tmdb.org/t/p/w500${movie.posterPath}",
+                          width: 100,
+                          height: 150,
+                          fit: BoxFit.cover,
+                        ),
+                        Text(
+                          movie.title.length > 14
+                              ? '${movie.title.substring(0, 10)}...'
+                              : movie.title,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              }),
+        )
+      ],
+    );
   }
 }
