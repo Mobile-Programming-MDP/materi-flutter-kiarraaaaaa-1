e61bee993f398ec4c672a36ea6b064aabd4098ca
kiarra <beneciakiarra_2226240052@mhs.mdp.ac.id>
2026-04-02 13:02:24 +0700

    update

--- cepu_app/lib/screens/home_screen.dart
+++ cepu_app/lib/screens/home_screen.dart
@@ -1,13 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'sign_in_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  Future<void> signOut(BuildContext context) async {
+    await FirebaseAuth.instance.signOut();
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (_) => const SignInScreen()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser;
+
     return Scaffold(
-      appBar: AppBar(title: const Text('Home Screen')),
-      body: const Center(child: Text("You Have Been Signed In!"),),
+      appBar: AppBar(
+        title: const Text("Home"),
+        actions: [
+          IconButton(
+            onPressed: () => signOut(context),
+            icon: const Icon(Icons.logout),
+          )
+        ],
+      ),
+      body: Center(
+        child: Text(
+          user != null
+              ? "Login sebagai: ${user.email}"
+              : "Tidak ada user",
+          style: const TextStyle(fontSize: 18),
+        ),
+      ),
     );
   }
 }
